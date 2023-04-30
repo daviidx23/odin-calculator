ec4528641d875bb2dd24042f2163f3a47bf4d737
daviidx23 <davidmas.sm@gmail.com>
2023-04-30 11:41:57 +0200

    Finish all functionalities and styles

--- script.js
+++ script.js
@@ -1,59 +1,100 @@
 function add(a,b){
-    return a+b;
+    return parseFloat(a)+parseFloat(b);
 }
 
 function subtract(a,b){
-    return a-b;
+    return parseFloat(a)-parseFloat(b);
 }
 
 function multiply(a,b){
-    return a*b;
+    return parseFloat(a)*parseFloat(b);
 }
 
 function divide(a,b){
-    return a/b;
+    return parseFloat(a)/parseFloat(b);
 }
 
-console.log(add(4,2));
-console.log(subtract(4,2));
-console.log(multiply(4,2));
-console.log(divide(4,2));
+let num1 = document.querySelector('#number1');
+let operator = document.querySelector('#operator');
+let num2 = document.querySelector('#number2');
+let answerZone = document.querySelector('#answer');
 
-let num1
-let operator;
-let num2;
-
-function operate(n2,op,n2){
+function operate(n1,op,n2){
     switch(op){
         case '+':
-            add(n1,n2);
+            return add(n1,n2);
             break;
         case '-':
-            subtract(n1,n2);
+            return subtract(n1,n2);
             break;
         case '*':
-            multiply(n1,n2);
+            return multiply(n1,n2);
             break;
         case '/':
-            divide(n1,n2);
+            return divide(n1,n2);
             break;
     }
 }
 
-const numbers = document.querySelectorAll('.numbers > div');
-
-function getNumbers(e,num1,num2){
-    if(num1==undefined){
-        num1 = e.currentTarget.innerText;
-    }
-    else {
-        num2 = e.currentTarget.innerText;
-    }
-    console.log(num1);
-    console.log(num2);
-}
+const numbers = document.querySelectorAll('#number');
 
 numbers.forEach(number => {
-    number.addEventListener('click', getNumbers, {once: true});
-    number.addEventListener('click', getNumbers, {once: true});
+    number.addEventListener('click', event => {
+        getNumbers(number.textContent);
+    });
 });
+
+function getNumbers(value){
+    if(operator.textContent==''){
+        num1.textContent += value;
+    }
+    else{
+        num2.textContent += value;
+    }
+}
+
+const operators = document.querySelectorAll('#operatorBtn');
+
+operators.forEach(operator => {
+    operator.addEventListener('click', event => {
+        getOperator(operator.textContent);
+    })
+});
+
+function getOperator(value){
+    if(num1.textContent != '' && num2.textContent == ''){
+        operator.textContent = value;
+    }
+    else if(num1.textContent != '' && num2.textContent != ''){
+        let res = operate(num1.textContent, operator.textContent, num2.textContent);
+        num1.textContent = res;
+        answerZone.textContent = res;
+        operator.textContent = value;
+        num2.textContent = '';
+    }
+    else if(num1.textContent == '' && answerZone.textContent != ''){
+        num1.textContent = answerZone.textContent;
+        operator.textContent = value;
+    }
+}
+
+const equalsBtn = document.querySelector('#equals');
+
+equalsBtn.onclick = function(){
+    if(num1.textContent != '' && operator.textContent != '' && num2.textContent != ''){
+        let answer = operate(num1.textContent, operator.textContent, num2.textContent);
+        answerZone.textContent = answer;
+        num1.textContent = '';
+        operator.textContent = '';
+        num2.textContent = '';
+    }
+}
+
+const clearBtn = document.querySelector('#clear');
+
+clearBtn.onclick = function(){
+    num1.textContent = '';
+    operator.textContent = '';
+    num2.textContent = '';
+    answerZone.textContent = '';
+}
